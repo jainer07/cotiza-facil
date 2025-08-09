430821b4d201308b4e3c3c448345c8cb19354589
Jainer Avila <jainer20-@hotmail.com>
2025-08-09 14:56:31 -0500

    ajuste de rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import { Dashboard } from "./components/dashboard/Dashboard";
 import './App.css';
@@ -7,12 +7,12 @@ function App() {
 
 	return (
 		<Container>
-			<BrowserRouter>
+			<HashRouter>
 				<Routes>
 					<Route path="/" element={<Dashboard />} />
 					<Route path="/dashboard/*" element={<Dashboard />} />
 				</Routes>
-			</BrowserRouter>
+			</HashRouter>
 		</Container >
 	)
 }
